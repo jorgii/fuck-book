dcdb576b8226402e388b1b15d192336e2d02b188
George Goranov <g.p.goranov@gmail.com>
2013-06-08 11:30:03 +0300

    Add model Person

--- potential_avenger/users/models.py
+++ potential_avenger/users/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class User1(models.Model):
-    name = models.CharField('Name', max_length=255)
+class Person(models.Model):
     user = models.OneToOneField(User)
+    name = models.CharField('Person Name', max_length=255)
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female')
+    )
+    gender = models.Field.choices(choices=GENDER_CHOICES)
+    birth_date = models.DateField()
+    city = models.CharField('City', max_length=255, null=True)
+    photo = models.FileField(upload_to='profile_photos')
