b9a445b6ec0a7913ea8e9b662fed1424a3c8086e
Stella Jivkova <stella.jivkova@gmail.com>
2013-06-08 13:29:53 +0300

    Add Location model

--- potential_avenger/checkin/models.py
+++ potential_avenger/checkin/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Location(models.Model):
+    user = models.OneToOneField(User)
+    latitude = models.DecimalField(max_digits=16, desimal_places=6)
+    longitude = models.DecimalField(max_digits=16, desimal_places=6)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
