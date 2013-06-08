04e51024c2906a76ac205b94182323a7225c4a2a
Stella Jivkova <stella.jivkova@gmail.com>
2013-06-08 14:25:05 +0300

    Add Notification model

--- potential_avenger/notifications/models.py
+++ potential_avenger/notifications/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from hardcoded_models.models import NotificationType
+
+
+class Notification (models.Model):
+    type_of_notification = models.ForeignKey(NotificationType)
+    user = models.OneToOneField(User)
+    timestamp = models.DateTimeField(auto_now=True, auto_now_add=False)
