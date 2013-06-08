a2ff0ade8aba3b0d16b098040c5480c9cbd6d6ed
George Goranov <g.p.goranov@gmail.com>
2013-06-08 13:58:25 +0300

    Add Diary model, insert to Admin panel

--- potential_avenger/diary/models.py
+++ potential_avenger/diary/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Diary(models.Model):
+    user1 = models.OneToOneField(User)
+    user2 = models.OneToOneField(User, related_name='related user')
+    timestamp = models.DateTimeField(auto_now_add=True)
