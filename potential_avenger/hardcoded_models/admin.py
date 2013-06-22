02f3c91111cf159a25c1d14c30448f63421e400c
Stella Jivkova <stella.jivkova@gmail.com>
2013-06-22 15:41:12 +0300

    Add NotificationType to Admin panel

--- potential_avenger/hardcoded_models/admin.py
+++ potential_avenger/hardcoded_models/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import PosesList, PlacesList, TipsList
+from .models import PosesList, PlacesList, TipsList, NotificationType
 
 
 admin.site.register(PosesList)
 admin.site.register(PlacesList)
 admin.site.register(TipsList)
+admin.site.register(NotificationType)
