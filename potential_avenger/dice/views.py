d6b04d64c834fb3ce9838207cfbf99afb9f1b2d6
Stella Jivkova <stella.jivkova@gmail.com>
2013-06-22 15:31:33 +0300

    Add pictures to Dice page

--- potential_avenger/dice/views.py
+++ potential_avenger/dice/views.py
@@ -11,13 +11,9 @@ from hardcoded_models.models import PosesList, PlacesList
 #@login_required
 def dice(request):
     poses_counter = PosesList.objects.count() - 1
-    print(poses_counter)
     index_poses = randint(0, poses_counter)
-    print(index_poses)
     random_pose = PosesList.objects.all()[index_poses]
     places_counter = PlacesList.objects.count() - 1
-    print(places_counter)
     index_places = randint(0, places_counter)
-    print(index_places)
     random_place = PlacesList.objects.all()[index_places]
     return render(request, "dice.html", locals())
