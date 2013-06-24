e6b058735eea415abba7385ce347a4d3582e42b1
Stella Jivkova <stella.jivkova@gmail.com>
2013-06-24 14:26:44 +0300

    Define Command that sands tip notification

--- potential_avenger/notifications/management/commands/send_periodical_notification.py
+++ potential_avenger/notifications/management/commands/send_periodical_notification.py
@@ -8,6 +8,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for this_person in PeriodicalNotification.person:
             this_person_notifications = PeriodicalNotification.objects.filter(person=this_person)
+            #To do: write an if-else statement for when there are no records in the db
             index = this_person_notifications.count() - 1
             last_entry = this_person_notifications.order_by('date')[index]
             if date.today() - last_entry == PeriodicalNotification.notification_period:
@@ -16,4 +17,4 @@ class Command(BaseCommand):
                     notification_period=PeriodicalNotification.notification_period,
                     message=PeriodicalNotification.message,
                     display=PeriodicalNotification.display,
-                    date=PeriodicalNotification.date.today())
+                    date_saved=date.today())
--- potential_avenger/notifications/management/commands/send_tip_notification.py
+++ potential_avenger/notifications/management/commands/send_tip_notification.py
@@ -1,6 +1,20 @@
-from django.core.management.base import BaseCommand, CommandError
-from notifications.models import Notification
+from datetime import date
+
+from django.core.management.base import BaseCommand
+from notifications.models import TipNotification
 
 
 class Command(BaseCommand):
-    
+    def handle(self, *args, **options):
+        for this_person in TipNotification.person:
+            this_person_notifications = TipNotification.objects.filter(person=this_person)
+            #To do: write an if-else statement for when there are no records in the db
+            index = this_person_notifications.count() - 1
+            last_entry = this_person_notifications.order_by('date')[index]
+            if date.today() - last_entry == TipNotification.notification_period:
+                TipNotification.objects.create(
+                    person=this_person,
+                    notification_period=TipNotification.notification_period,
+                    message=TipNotification.message,
+                    display=TipNotification.display,
+                    date_saved=date.today())
