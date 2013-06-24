25f66d96fc44c9f40b43d711684caef6b4d5f500
Stella Jivkova <stella.jivkova@gmail.com>
2013-06-24 14:17:13 +0300

    Define Command that sends periodical notification

--- potential_avenger/notifications/management/commands/send_periodical_notification.py
+++ potential_avenger/notifications/management/commands/send_periodical_notification.py
@@ -1,6 +1,19 @@
-from django.core.management.base import BaseCommand, CommandError
-from notifications.models import Notification
+from datetime import date
+
+from django.core.management.base import BaseCommand
+from notifications.models import PeriodicalNotification
 
 
 class Command(BaseCommand):
-    
+    def handle(self, *args, **options):
+        for this_person in PeriodicalNotification.person:
+            this_person_notifications = PeriodicalNotification.objects.filter(person=this_person)
+            index = this_person_notifications.count() - 1
+            last_entry = this_person_notifications.order_by('date')[index]
+            if date.today() - last_entry == PeriodicalNotification.notification_period:
+                PeriodicalNotification.objects.create(
+                    person=this_person,
+                    notification_period=PeriodicalNotification.notification_period,
+                    message=PeriodicalNotification.message,
+                    display=PeriodicalNotification.display,
+                    date=PeriodicalNotification.date.today())
