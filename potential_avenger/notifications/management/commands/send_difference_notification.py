5aca80e30aaf98d08f16ed6ea7427fc1b615c8de
Stella Jivkova <stella.jivkova@gmail.com>
2013-06-24 14:29:52 +0300

    Define Command that sends difference notification

--- potential_avenger/notifications/management/commands/send_difference_notification.py
+++ potential_avenger/notifications/management/commands/send_difference_notification.py
@@ -1,6 +1,20 @@
-from django.core.management.base import BaseCommand, CommandError
-from notifications.models import Notification
+from datetime import date
+
+from django.core.management.base import BaseCommand
+from notifications.models import DifferenceNotification
 
 
 class Command(BaseCommand):
-    
+    def handle(self, *args, **options):
+        for this_person in DifferenceNotification.person:
+            this_person_notifications = DifferenceNotification.objects.filter(person=this_person)
+            #To do: write an if-else statement for when there are no records in the db
+            index = this_person_notifications.count() - 1
+            last_entry = this_person_notifications.order_by('date')[index]
+            if date.today() - last_entry == DifferenceNotification.notification_period:
+                DifferenceNotification.objects.create(
+                    person=this_person,
+                    notification_period=DifferenceNotification.notification_period,
+                    message=DifferenceNotification.message,
+                    display=DifferenceNotification.display,
+                    date_saved=date.today())
