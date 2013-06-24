115a22748a350ba693371d73ac1a7dda5db89be1
Stella Jivkova <stella.jivkova@gmail.com>
2013-06-24 14:15:08 +0300

    Remove Notification model
    Remove Notification model and create different models for the different
    types of notifications

--- potential_avenger/notifications/models.py
+++ potential_avenger/notifications/models.py
@@ -1,12 +1,44 @@
+from datetime import date
+from random import randint
+
 from django.db import models
 
 from users.models import Person
-from hardcoded_models.models import NotificationType
+from hardcoded_models.models import TipsList
 
 
-class Notification (models.Model):
-    type_of_notification = models.ForeignKey(NotificationType)
+class PeriodicalNotification (models.Model):
+    person = models.OneToOneField(Person)
+    notification_period = models.IntegerField(default=7)
+    message = models.CharField(
+        'Notification Message',
+        max_length=255,
+        default="Haven't checked-in in a while. Sex life getting slow?")
+    display = models.BooleanField(default=True)
+    date_saved = models.DateField(default=date.today())
+
+
+class TipNotification (models.Model):
     person = models.OneToOneField(Person)
     notification_period = models.IntegerField(default=7)
     message = models.CharField('Notification Message', max_length=255)
     display = models.BooleanField(default=True)
+    date_saved = models.DateField(default=date.today())
+
+    def __init__(self, message):
+        tips_counter = TipsList.objects.count() - 1
+        index_tips = randint(0, tips_counter)
+        self.message = TipsList.objects.all()[index_tips]
+        return self.message
+
+
+class DifferenceNotification (models.Model):
+    person = models.OneToOneField(Person)
+    notification_period = models.IntegerField(default=7)
+    message = models.CharField('Notification Message', max_length=255)
+    display = models.BooleanField(default=True)
+    date_saved = models.DateField(default=date.today())
+
+    def __init__(self, message):
+        self.message = " "
+        return self.message
