40f4cf3d88718dd67f13cf4fa3a10f117d43e8b8
George Goranov <g.p.goranov@gmail.com>
2013-06-25 19:33:06 +0300

    add diary veiw and html template

--- potential_avenger/diary/views.py
+++ potential_avenger/diary/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
+from diary.models import Diary
+
+
+@login_required
 def diary(request):
+    diary_list = Diary.objects.filter(person1=request.user.person)
+    persons_list = []
+    for diary in diary_list:
+        persons_list.append([str(diary.person1), str(diary.person2)])
     return render(request, "diary.html", locals())
