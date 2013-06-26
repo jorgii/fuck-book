5d44cd8727f05dcc83d0d7eb14d7f3527ece152f
Stella Jivkova <stella.jivkova@gmail.com>
2013-06-26 16:50:27 +0300

    Fix CheckinDetails model

--- potential_avenger/checkin/views.py
+++ potential_avenger/checkin/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
 
+from checkin.models import CheckinDetails
 from checkin.forms import CheckinForm
 
 
 @login_required
 def checkin(request):
-    checkin_form = CheckinForm(request.POST or None)
+    current_person = CheckinDetails(person=request.user.person)
+    checkin_form = CheckinForm(request.POST or None, instance=current_person)
     if request.method == 'POST':
         if checkin_form.is_valid():
             checkin_form.save()
