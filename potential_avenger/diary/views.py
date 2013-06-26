334d5996845ae78ed0d490f04bd506b5b6bb387d
George Goranov <g.p.goranov@gmail.com>
2013-06-26 22:35:45 +0300

    Enhance diary app.
    person2 is not mandatory
    
    View and Template show all checkins by diary for all diaries.

--- potential_avenger/diary/views.py
+++ potential_avenger/diary/views.py
@@ -1,14 +1,28 @@
+from collections import OrderedDict
+
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
 from diary.models import Diary
+from checkin.models import CheckinDetails
 
 
 @login_required
 def diary(request):
-    diary_list = Diary.objects.filter(person1=request.user.person)
-    persons_list = []
+    checkins_by_diary = OrderedDict()
+    diary_list = list(Diary.objects.filter(person1=request.user.person))
+    diary_list.extend(list(Diary.objects.filter(person2=request.user.person)))
+    diary_list = sorted(diary_list, key=lambda x: x.timestamp, reverse=True)
     for diary in diary_list:
-        persons_list.append([str(diary.person1), str(diary.person2)])
+        checkins_by_diary[diary] = get_checkins_for_diary(diary)
     return render(request, "diary.html", locals())
+
+
+def get_checkins_for_diary(diary):
+    checkins = list(CheckinDetails.objects.filter(person=diary.person1,
+                                                  with_who=diary.person2))
+    checkins.extend(CheckinDetails.objects.filter(person=diary.person2,
+                                                  with_who=diary.person1))
+    return sorted(checkins, key=lambda x: x.date_checked, reverse=True)
