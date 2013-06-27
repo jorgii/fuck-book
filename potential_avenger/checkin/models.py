802bf4088ad3fd30df05faf8f90713196b2c0b14
Stella Jivkova <stella.jivkova@gmail.com>
2013-06-27 20:17:14 +0300

    Fix CheckinDetails model .. again

--- potential_avenger/checkin/models.py
+++ potential_avenger/checkin/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 from hardcoded_models.models import PosesList, PlacesList
@@ -6,7 +8,7 @@ from users.models import Person
 
 class CheckinDetails(models.Model):
     person = models.ForeignKey(Person)
-    date_checked = models.DateTimeField(auto_now_add=True)
+    date_checked = models.DateField(default=date.today())
     address = models.CharField('Address', max_length=255)
     poses = models.ManyToManyField(PosesList)
     places = models.ManyToManyField(PlacesList)
