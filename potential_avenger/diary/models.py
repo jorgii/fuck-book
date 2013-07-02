638c1b759abddc8f231e409ac59c7b82270282c1
George Goranov <g.p.goranov@gmail.com>
2013-07-02 11:54:37 +0300

    Add __str__() to diary model

--- potential_avenger/diary/models.py
+++ potential_avenger/diary/models.py
@@ -8,3 +8,9 @@ class Diary(models.Model):
     person1 = models.ForeignKey(Person)
     person2 = models.ForeignKey(Person, related_name='diary related user', blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.person2:
+            return '{}, {}, {}'.format(self.timestamp, str(self.person1.user.username), str(self.person2.user.username))
+        else:
+            return '{}, {}, {}'.format(self.timestamp, str(self.person1.user.username), None)
