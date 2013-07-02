bd5ebc94ba18d64081babb29bcefa5bb0a9a4153
George Goranov <g.p.goranov@gmail.com>
2013-07-02 18:39:08 +0300

    Add test  for diary listing

--- potential_avenger/diary/tests.py
+++ potential_avenger/diary/tests.py
@@ -17,13 +17,22 @@ class DiaryTest(TestCase):
         self.person2 = User.objects.get(username='user2').person
 
     def test_get_checkins_for_diary(self):
-        diary = Diary.objects.get(person1=self.person1, person2=self.person2)
+        intended_diary = Diary.objects.get(person1=self.person1, person2=self.person2)
         expected_checkins = list(CheckinDetails.objects.filter(person=self.person1,
                                                                with_who=self.person2))
         expected_checkins.extend(CheckinDetails.objects.filter(person=self.person2,
                                                                with_who=self.person1))
         expected_checkins = sorted(expected_checkins, key=lambda x: x.date_checked, reverse=True)
-        self.assertEqual(expected_checkins, get_checkins_for_diary(diary))
+        self.assertEqual(expected_checkins, get_checkins_for_diary(intended_diary))
+
+    def test_diary_list_view(self):
+        self.client.login(username='user1', password='pass1')
+        expected_diary_list = list(Diary.objects.filter(person1=self.person1))
+        expected_diary_list.extend(list(Diary.objects.filter(person2=self.person1)))
+        expected_diary_list = sorted(expected_diary_list, key=lambda x: x.timestamp, reverse=True)
+        response = self.client.get('/diary/')
+        received_diary_list = response.context['diary_list']
+        self.assertEqual(expected_diary_list, received_diary_list)
 
     def test_diary_get(self):
         self.client.login(username='user1', password='pass1')
