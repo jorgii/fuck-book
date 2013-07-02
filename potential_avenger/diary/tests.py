6433cc8f92d58bab413756cc7e2d79bbccd87108
George Goranov <g.p.goranov@gmail.com>
2013-07-02 16:43:58 +0300

    Add test for get_checkins_by_diary

--- potential_avenger/diary/tests.py
+++ potential_avenger/diary/tests.py
@@ -1,16 +1,27 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.test.client import Client
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from diary.models import Diary
+from diary.views import get_checkins_for_diary
+from checkin.models import CheckinDetails
+
+
+class DiaryTest(TestCase):
+    fixtures = ['users_data.json', 'persons_data.json', 'diary_data.json', 'checkin_data.json', 'hardcoded_data.json']
+
+    def setUp(self):
+        self.client = Client()
+        self.person1 = User.objects.get(username='user1').person
+        self.person2 = User.objects.get(username='user2').person
+
+    def test_get_checkins_for_diary(self):
+        import ipdb; ipdb.set_trace()
+        diary = Diary.objects.get(person1=self.person1, person2=self.person2)
+        expected_checkins = list(CheckinDetails.objects.filter(person=self.person1,
+                                                               with_who=self.person2))
+        expected_checkins.extend(CheckinDetails.objects.filter(person=self.person2,
+                                                               with_who=self.person1))
+        expected_checkins = sorted(expected_checkins, key=lambda x: x.date_checked, reverse=True)
+        self.assertEqual(expected_checkins, get_checkins_for_diary(diary))
