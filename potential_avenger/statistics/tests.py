b5beddc14764f03c20a6a6d89d35a3d51657e20a
George Goranov <g.p.goranov@gmail.com>
2013-07-03 18:31:05 +0300

    Add get_checkins_grouped test

--- potential_avenger/statistics/tests.py
+++ potential_avenger/statistics/tests.py
@@ -1,16 +1,54 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+from datetime import date
+from collections import OrderedDict
 
-Replace this with more appropriate tests for your application.
-"""
 
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.test.client import Client
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from users.models import Person
+from checkin.models import CheckinDetails
+from statistics.views import get_checkins_grouped
+
+
+class StatisticsTest(TestCase):
+    fixtures = ['users_data.json', 'persons_data.json']
+
+    def setUp(self):
+        self.client = Client()
+        self.person1 = Person.objects.get(user=User.objects.get(username='user1'))
+
+    def test_get_checkins_grouped_daily(self):
+        expected_daily_checkins = OrderedDict()
+        expected_daily_checkins[date(year=2013, month=3, day=3)] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2013, month=3, day=3))]
+        expected_daily_checkins[date(year=2013, month=2, day=2)] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2013, month=2, day=2))]
+        expected_daily_checkins[date(year=2013, month=1, day=1)] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2013, month=1, day=1))]
+        expected_daily_checkins[date(year=2012, month=4, day=3)] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2012, month=4, day=3))]
+        expected_daily_checkins[date(year=2012, month=3, day=2)] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2012, month=3, day=2))]
+        expected_daily_checkins[date(year=2012, month=2, day=1)] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2012, month=2, day=1))]
+        expected_daily_checkins[date(year=2012, month=1, day=3)] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2012, month=1, day=3))]
+        expected_daily_checkins[date(year=2012, month=1, day=2)] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2012, month=1, day=2))]
+        expected_daily_checkins[date(year=2012, month=1, day=1)] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2012, month=1, day=1))]
+        result_daily_checkins = get_checkins_grouped(person=self.person1,
+                                                     from_t=date(year=2012, month=1, day=1),
+                                                     to_t=date(year=2013, month=3, day=3),
+                                                     group_by='d')
+        self.assertEqual(expected_daily_checkins, result_daily_checkins)
+
+    def test_get_checkins_grouped_monthly(self):
+        expected_monthly_checkins = OrderedDict()
+        expected_monthly_checkins['3/2013'] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2013, month=3, day=3))]
+        expected_monthly_checkins['2/2013'] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2013, month=2, day=2))]
+        expected_monthly_checkins['1/2013'] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2013, month=1, day=1))]
+        expected_monthly_checkins['4/2012'] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2012, month=4, day=3))]
+        expected_monthly_checkins['3/2012'] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2012, month=3, day=2))]
+        expected_monthly_checkins['2/2012'] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2012, month=2, day=1))]
+        expected_monthly_checkins['1/2012'] = [CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2012, month=1, day=3)),
+                                               CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2012, month=1, day=2)),
+                                               CheckinDetails.objects.create(person=self.person1, address='Sofia', rating=1, date_checked=date(year=2012, month=1, day=1))]
+        result_monthly_checkins = get_checkins_grouped(person=self.person1,
+                                                       from_t=date(year=2012, month=1, day=1),
+                                                       to_t=date(year=2013, month=3, day=3),
+                                                       group_by='m')
+        self.assertEqual(expected_monthly_checkins, result_monthly_checkins)
