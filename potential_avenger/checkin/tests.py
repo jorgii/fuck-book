d0bdbf4573694552e6e6c38ac8fda7947ab07160
Stella Jivkova <stella.jivkova@gmail.com>
2013-07-04 16:15:58 +0300

    Add first working test to checkin tests

--- potential_avenger/checkin/tests.py
+++ potential_avenger/checkin/tests.py
@@ -9,54 +9,14 @@ from users.models import Person
 
 
 class CheckinTest(TestCase):
+    fixtures = ['users_data.json', 'persons_data.json', 'checkin_data.json']
+
     def setUp(self):
         self.client = Client()
+        self.person1 = User.objects.get(username='user1').person
+        self.person2 = User.objects.get(username='user2').person
+        self.checkin = CheckinDetails.objects.get(id=1)
 
-        self.person = self.create_person(user=self.create_user())
-        # self.person_preferences = self.create_person_preferences(person=self.person)
-        # self.person_personal_settings = self.create_personal_settings(person=self.person)
-        # self.person_personal_settings.save()
-        # self.person_preferences.save()
-        self.person.save()
-        self.checkin = slef.create_checkin(person=self.person)
-        self.checkin.save()
-
-    def create_user(self,
-                    username='user1',
-                    password='pass1',
-                    first_name='user1 name1',
-                    last_name='user1 name1',):
-        return User.objects.create_user(username=username,
-                                        password=password,
-                                        first_name=first_name,
-                                        last_name=last_name,)
-
-    def create_person(self,
-                      user,
-                      gender='M',
-                      birth_date=date.today(),
-                      city='Sofia'):
-        return Person.objects.create(user=user,
-                                     gender=gender,
-                                     birth_date=birth_date,
-                                     city=city,)
-
-    def create_checkin(self,
-                       person,
-                       date_checked,
-                       address='Cherni vrah 47, Sofia, Bulgaria',
-                       poses=list('missionary', 'leg_lock', 'advanced_sex'),
-                       places=list('doube_bed', 'desk'),
-                       raiting='4',
-                       duration,
-                       contraception,
-                       with_who=None):
-        return CheckinDetails.objects.create(person=person,
-                                             date_checked=date_checked,
-                                             address=address,
-                                             poses=poses,
-                                             places=places,
-                                             raiting=raiting,
-                                             duration=duration,
-                                             contraception=contraception,
-                                             with_who=with_who,)
+    def test_create_checkin(self):
+        self.assertTrue(isinstance(self.checkin, CheckinDetails))
+        self.assertEqual(str(self.checkin), str(self.checkin.date_checked) + ', ' + str(self.checkin.person.user))
