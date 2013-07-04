7135ad78c7d9a20fa2c605561353303caff6622d
Stella Jivkova <stella.jivkova@gmail.com>
2013-07-04 17:00:09 +0300

    Add changes to CheckinForm

--- potential_avenger/checkin/forms.py
+++ potential_avenger/checkin/forms.py
@@ -5,6 +5,11 @@ from checkin.models import CheckinDetails
 
 
 class CheckinForm(ModelForm):
+    def __init__(self, data=None, instance=None, *args, **kwargs):
+        super(CheckinForm, self).__init__(data=data, instance=instance, *args, **kwargs)
+        with_who_set = self.fields['with_who'].queryset
+        self.fields['with_who'].queryset = with_who_set.exclude(id=instance.person.id)
+
     class Meta:
         model = CheckinDetails
         fields = [
