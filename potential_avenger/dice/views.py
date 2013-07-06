9217eb64fcba5cda5e38a7cabc81c808d7bbba16
George Goranov <g.p.goranov@gmail.com>
2013-07-06 11:31:09 +0300

    Fix dice view

--- potential_avenger/dice/views.py
+++ potential_avenger/dice/views.py
@@ -10,15 +10,14 @@ from hardcoded_models.models import PosesList, PlacesList
 
 @login_required
 def dice(request):
+    ERROR_MESSAGE = {'pose': "Sorry, no available poses to choose from.",
+                     'place': "Sorry, no available places to choose from."}
     try:
         random_pose = random.choice(PosesList.objects.all())
     except IndexError:
-        random_pose = dice.ERROR_MESSAGE['pose']
+        random_pose = ERROR_MESSAGE['pose']
     try:
         random_place = random.choice(PlacesList.objects.all())
     except IndexError:
-        random_place = dice.ERROR_MESSAGE['place']
+        random_place = ERROR_MESSAGE['place']
     return render(request, "dice.html", locals())
-
-    ERROR_MESSAGE = {'pose': "Sorry, no available poses to choose from.",
-                     'place': "Sorry, no available places to choose from."}
