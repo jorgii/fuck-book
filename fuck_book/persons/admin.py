a22836ae8f9a4c82fcf3a3a86bee5e786536b39c
George Goranov <g.p.goranov@gmail.com>
2013-11-25 17:20:04 +0200

    Remove the deleted models

--- fuck_book/persons/admin.py
+++ fuck_book/persons/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
-from .models import Person, PersonPreferences, PersonalSettings
+from .models import Person
 
 
 admin.site.register(Person)
-admin.site.register(PersonPreferences)
-admin.site.register(PersonalSettings)
