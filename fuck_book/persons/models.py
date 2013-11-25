e867ab8ae4e89e5ee869b3af61e43883d8caefa7
George Goranov <g.p.goranov@gmail.com>
2013-11-25 17:19:29 +0200

    Merge Person model

--- fuck_book/persons/models.py
+++ fuck_book/persons/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.core.exceptions import ValidationError
-from fuck_book import settings
 
 
 def get_upload_file_name(instance, filename):
@@ -17,13 +16,6 @@ class Person(models.Model):
     birth_date = models.DateField(blank=True, null=True)
     city = models.CharField('City', max_length=255, blank=True, null=True)
     photo = models.FileField(upload_to=get_upload_file_name, verbose_name="Profile photo", blank=True, null=True)
-
-    def __str__(self):
-        return '{} {}'.format(self.user.first_name, self.user.last_name)
-
-
-class PersonPreferences(models.Model):
-    person = models.OneToOneField('persons.Person')
     relation = models.OneToOneField('persons.Person',
                                     related_name='related user',
                                     blank=True,
@@ -31,38 +23,6 @@ class PersonPreferences(models.Model):
                                     error_messages={'unique': 'This person is already in a relation with another user!'})
     preferred_poses = models.ManyToManyField('hardcoded_models.PosesList', blank=True, null=True)
     preferred_places = models.ManyToManyField('hardcoded_models.PlacesList', blank=True, null=True)
-
-    def __str__(self):
-        return self.person.__str__()
-
-    def save(self, *args, **kwargs):
-        '''Redefined save method to handle creation of the relation between 2 Persons.
-        It sets the relation for both Persons.
-        Works in the oposite direction, removes relation for both persons
-
-        '''
-        super(PersonPreferences, self).save(*args, **kwargs)
-
-        if self.relation and self.relation.personpreferences.relation != self.person:
-            self.relation.personpreferences.relation = self.person
-            self.relation.personpreferences.save()
-
-        if not self.relation and Person.objects.filter(personpreferences__relation=self.person).exists():
-            other_person = Person.objects.get(personpreferences__relation=self.person)
-            other_person.personpreferences.relation = None
-            other_person.personpreferences.save()
-
-    def clean(self):
-        '''Redefined clean method to make sure relation cannot be set to self.
-
-        '''
-        super(PersonPreferences, self).clean()
-        if self.person == self.relation:
-            raise ValidationError('One person cannot be related to itself')
-
-
-class PersonalSettings(models.Model):
-    person = models.OneToOneField('persons.Person')
     display_periodical_notification = models.BooleanField(default=True)
     display_tip_notification = models.BooleanField(default=True)
     display_difference_notification = models.BooleanField(default=True)
@@ -71,5 +31,29 @@ class PersonalSettings(models.Model):
     tip_notification_period = models.IntegerField(default=7)
     difference_notification_period = models.IntegerField(default=30)
 
+    def save(self, *args, **kwargs):
+        '''Redefined save method to handle creation of the relation between 2 Persons.
+        It sets the relation for both Persons.
+        Works in the oposite direction, removes relation for both persons
+
+        '''
+        super(Person, self).save(*args, **kwargs)
+
+        if self.relation and self.relation.relation != self:
+            self.relation.relation = self
+            self.relation.save()
+
+        if not self.relation and Person.objects.filter(relation=self).exists():
+            other_person = Person.objects.get(relation=self)
+            other_person.relation = None
+            other_person.save()
+
+    def clean(self):
+        '''Redefined clean method to make sure relation cannot be set to self.
+
+        '''
+        super(Person, self).clean()
+        if self == self.relation:
+            raise ValidationError('One person cannot be related to itself')
     def __str__(self):
-        return self.person.__str__()
+        return '{} {}'.format(self.user.first_name, self.user.last_name)
