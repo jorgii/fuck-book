707386d1f567d24118627c20fb398f3d93e60746
George Goranov <g.p.goranov@gmail.com>
2013-11-26 18:01:21 +0200

    Align with new Persons model

--- fuck_book/checkin/views.py
+++ fuck_book/checkin/views.py
@@ -26,7 +26,7 @@ def checkin(request):
             if not person1_person2_diary:
                 Diary.objects.create(person1=request.user.person, person2=checkin_form.instance.with_who)
 
-            this_person_settings = Person.objects.get(person=this_person)
+            this_person_settings = Person.objects.get(id=this_person.id)
             if this_person_settings.display_periodical_notification:
                 update_last_periodical_notification(this_person)
 
