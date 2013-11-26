188e902aeae6e2a433898a39ace0f5a1426ebcce
George Goranov <g.p.goranov@gmail.com>
2013-11-26 18:01:32 +0200

    Align with new Persons model

--- fuck_book/notifications/management/commands/send_tip_notification.py
+++ fuck_book/notifications/management/commands/send_tip_notification.py
@@ -1,17 +1,16 @@
 from django.core.management.base import BaseCommand
 
 from notifications.models import TipNotification
-from persons.models import Person, PersonalSettings
+from persons.models import Person
 from notifications.management.commands.send_difference_notification import get_most_recent_notification, time_to_send_notification
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for this_person in Person.objects.all():
-            this_person_settings = PersonalSettings.objects.get(person=this_person)
 
-            if this_person_settings.display_tip_notification:
+            if this_person.display_tip_notification:
                 last_entry = get_most_recent_notification(this_person, TipNotification)
 
-                if time_to_send_notification(last_entry, this_person_settings.tip_notification_period):
+                if time_to_send_notification(last_entry, this_person.tip_notification_period):
                     TipNotification.objects.create(person=this_person)
