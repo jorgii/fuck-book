6b45b890662a38af6f1fba5074929febdc26a51e
George Goranov <g.p.goranov@gmail.com>
2013-12-03 11:54:27 +0200

    Transform with_who to ManyToMany
    You can checking with multiple persons

--- fuck_book/checkin/models.py
+++ fuck_book/checkin/models.py
@@ -7,6 +7,8 @@ from django.core.exceptions import ValidationError
 class CheckinDetails(models.Model):
     person = models.ForeignKey('persons.Person')
     date_checked = models.DateField(default=date.today())
+    longitude = models.DecimalField('Longitude', max_digits=9, decimal_places=6 ,null=True, blank=True)
+    latitude = models.DecimalField('Latitude', max_digits=9, decimal_places=6, null=True, blank=True)
     address = models.CharField('Address (ex: Cherni vrah 47, Sofia, Bulgaria):', max_length=255)
     poses = models.ManyToManyField('hardcoded_models.PosesList', blank=True, null=True)
     places = models.ManyToManyField('hardcoded_models.PlacesList', blank=True, null=True)
@@ -20,7 +22,7 @@ class CheckinDetails(models.Model):
     rating = models.CharField(max_length=1, choices=RAITING_CHOICES)
     duration = models.IntegerField('Duration (in minutes):', default=10)
     contraception = models.BooleanField('Contraception (y/n):', default=True)
-    with_who = models.ForeignKey('persons.Person', related_name='checkin_related_user', blank=True, null=True)
+    with_who = models.ManyToManyField('persons.Person', related_name='checkin_related_users', blank=True, null=True)
 
     def __str__(self):
         return '{}, {}'.format(self.date_checked, self.person.user)
@@ -32,7 +34,7 @@ class CheckinDetails(models.Model):
         - person cannot checkin with too large duration number
         '''
         super(CheckinDetails, self).clean()
-        if self.person == self.with_who:
+        if self.person in self.with_who.all():
             raise ValidationError("'Loving' yourself might be great, but that's not what we're looking for here. ;)")
         if self.duration <= 0:
             raise ValidationError("Man! Either you were faster than the light's speed or you entered the wrong number. Try positive values the next time.")
