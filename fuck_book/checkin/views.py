dbf0f056ccb2826ef2dc54535954abb72cac0258
George Goranov <g.p.goranov@gmail.com>
2013-12-04 23:11:43 +0200

    Use Diary methods to access the model

--- fuck_book/checkin/views.py
+++ fuck_book/checkin/views.py
@@ -14,17 +14,18 @@ from diary.models import Diary
 @login_required
 def checkin(request):
     this_person = request.user.person
-    current_checkin = CheckinDetails(person=this_person)
+    current_checkin = CheckinDetails(creator=this_person)
     checkin_form = CheckinForm(request.POST or None, instance=current_checkin)
 
     if request.method == 'POST':
         if checkin_form.is_valid():
             checkin_form.save()
 
-            person1_person2_diary = Diary.objects.filter(person1=this_person, person2=checkin_form.instance.with_who) \
-                or Diary.objects.filter(person1=checkin_form.instance.with_who, person2=this_person)
-            if not person1_person2_diary:
-                Diary.objects.create(person1=request.user.person, person2=checkin_form.instance.with_who)
+            attached_diary = Diary.get_diary_for_exact_people(*list(current_checkin.participants.all()))
+            if attached_diary:
+                attached_diary.add_checkins_to_diary(current_checkin)
+            else:
+                Diary.create(creator=current_checkin.creator, participants=list(current_checkin.participants.all()), checkins=current_checkin)
 
             this_person_settings = Person.objects.get(id=this_person.id)
             if this_person_settings.display_periodical_notification:
