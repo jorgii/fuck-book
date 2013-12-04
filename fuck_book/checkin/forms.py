a62b801110b9633ea7f7d9fbfc5c3b0ae1de8146
George Goranov <g.p.goranov@gmail.com>
2013-12-04 23:08:50 +0200

    Include self in checkin form

--- fuck_book/checkin/forms.py
+++ fuck_book/checkin/forms.py
@@ -5,10 +5,6 @@ from checkin.models import CheckinDetails
 
 
 class CheckinForm(ModelForm):
-    def __init__(self, data=None, instance=None, *args, **kwargs):
-        super(CheckinForm, self).__init__(data=data, instance=instance, *args, **kwargs)
-        with_who_set = self.fields['participants'].queryset
-        self.fields['participants'].queryset = with_who_set.exclude(id=instance.person.id)
 
     class Meta:
         model = CheckinDetails
