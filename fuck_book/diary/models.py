58e7d5459f22201d3a32afe800b4ffa157264b55
George Goranov <g.p.goranov@gmail.com>
2013-12-04 23:10:43 +0200

    Add checkins adding method

--- fuck_book/diary/models.py
+++ fuck_book/diary/models.py
@@ -3,21 +3,26 @@ from django.db import models
 
 class Diary(models.Model):
     creator = models.ForeignKey('persons.Person')
-    participants = models.ManyToManyField('persons.Person', related_name='diary participants', blank=True, null=True)
+    participants = models.ManyToManyField('persons.Person', related_name='diary_participants', blank=True, null=True)
     checkins = models.ManyToManyField('checkin.CheckinDetails')
     datetime_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return 'DateTime: {},Creator: {},Participants: {}'.format(self.datetime_created, str(self.creator.user.username), [str(p.user.username) for p in self.participants.all()])
 
+    def add_checkins_to_diary(self, *checkins):
+        diary_to_edit = Diary.objects.get(id=self.id)
+        for checkin in checkins:
+            diary_to_edit.checkins.add(checkin)
+        diary_to_edit.save()
+        return
 
     @staticmethod
-    def get_diary_for_exact_people(*people):
-        gathered_diaries = Diary.get_diaries_for_people(*people)
-        for diary in gathered_diaries:
-            if diary.participants.count() != len(people):
-                gathered_diaries = gathered_diaries.exclude(id=diary.id)
-        return gathered_diaries
+    def get_diary_for_exact_people(*people): 
+        for diary in Diary.get_diaries_for_people(*people):
+            if diary.participants.count() == len(people):
+                return diary
+        return
 
 
     @staticmethod
