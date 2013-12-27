1e195004295c476c8e35f2f344fc53c3825fde55
George Goranov <g.p.goranov@gmail.com>
2013-12-27 11:16:58 +0200

    Add test data

--- fuck_book/diary/tests.py
+++ fuck_book/diary/tests.py
@@ -4,7 +4,7 @@ from django.test.client import Client
 
 
 from diary.models import Diary
-
+from checkin.models import CheckinDetails
 
 class DiaryTest(TestCase):
     fixtures = ['users_data.json', 'persons_data.json']
@@ -13,9 +13,32 @@ class DiaryTest(TestCase):
         self.client = Client()
         self.person1 = User.objects.get(username='user1').person
         self.person2 = User.objects.get(username='user2').person
+        self.person3 = User.objects.get(username='user3').person
+        self.person4 = User.objects.get(username='user4').person
+        self.checkin1 = CheckinDetails.objects.create(creator=self.person1, rating=3, duration=30)
+        self.checkin1.participants.add(self.person1, self.person2, self.person3)
+        self.checkin2 = CheckinDetails.objects.create(creator=self.person2, rating=3, duration=30)
+        self.checkin2.participants.add(self.person1, self.person2, self.person3)
+        self.checkin3 = CheckinDetails.objects.create(creator=self.person3, rating=3, duration=30)
+        self.checkin3.participants.add(self.person1, self.person2, self.person3)
+        self.checkin4 = CheckinDetails.objects.create(creator=self.person4, rating=3, duration=30)
+        self.checkin4.participants.add(self.person1, self.person2, self.person4)
+        self.checkin5 = CheckinDetails.objects.create(creator=self.person1, rating=3, duration=30)
+        self.checkin5.participants.add(self.person1, self.person2, self.person4)
+        self.empty_checkin = CheckinDetails.objects.create(creator=self.person1, rating=3, duration=30)
+        self.empty_checkin.participants.add(self.person1)
+        self.diary = Diary.objects.create(creator=self.person1)
+        self.diary1.checkins.add(self.checkin1, self.checkin2, self.checkin3)
+        self.diary1.participants.add(self.person1, self.person2, self.person3)
+        self.diary2 = Diary.objects.create(creator=self.person4)
+        self.diary1.checkins.add(self.checkin4, self.checkin5)
+        self.diary2.participants.add(self.person1, self.person2, self.person4)
+        self.empty_diary = Diary.objects.create(creator=self.person1)
+        self.empty_diary.participants.add(self.person1)
+
 
     def test_diary_str(self):
-        diary = Diary.objects.get(person1=self.person1, person2=self.person2)
+        diary = Diary.objects.get(creator=self.person1, participants=[self.person1, self.person2, self.person3])
         self.assertEqual(str(diary), 'DateTime: {},Creator: {},Participants: {}'.format(diary.datetime_created, str(diary.creator.user.username), [str(p.user.username) for p in diary.participants.all()]))
 
     def test_diary_list_view(self):
