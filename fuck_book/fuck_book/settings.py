ce6c506b5bd5a00f659e700621c73de3349d7e98
George Goranov <g.p.goranov@gmail.com>
2014-03-18 17:44:21 +0200

    Add json_field

--- fuck_book/fuck_book/settings.py
+++ fuck_book/fuck_book/settings.py
@@ -149,6 +149,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'django_extensions',
+    'json_field',
     'storages',
     'imagekit',
     'south',
