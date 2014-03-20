4138fea2601c809609b49da3d2524957ab6537b8
George Goranov <g.p.goranov@gmail.com>
2014-03-20 16:02:44 +0200

    Remove last periodical notification update method
    This is not needed anymore since the command will directly take the
    latest checkin.

--- fuck_book/checkin/views.py
+++ fuck_book/checkin/views.py
@@ -1,12 +1,9 @@
-from datetime import date
-
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
 
 from checkin.models import CheckinDetails
 from checkin.forms import CheckinForm
-from notifications.models import PeriodicalNotification
 from persons.models import Person
 from diary.models import Diary
 
@@ -28,21 +25,7 @@ def checkin(request):
                 Diary.create(creator=current_checkin.creator, participants=list(current_checkin.participants.all()), checkins=current_checkin)
 
             this_person_settings = Person.objects.get(id=this_person.id)
-            if this_person_settings.display_periodical_notification:
-                update_last_periodical_notification(this_person)
 
             return redirect('/diary/')
     csrf(request)
     return render(request, "checkin.html", locals())
-
-
-def update_last_periodical_notification(person):
-    ''' Takes one parameter - the curren logged person
-    Finds the last periodical notification this person got
-    and updates the date_saved of this notification to the
-    date of the new chekin made in this request.
-    '''
-    this_person_notifications = PeriodicalNotification.objects.filter(person=person)
-    last_entry = this_person_notifications.latest('date_saved')
-    last_entry.date_saved = date.today()
-    last_entry.save(update_fields=['date_saved'])
