5103464c465c1696f61a513f635b5f0dcde60e8c
George Goranov <g.p.goranov@gmail.com>
2014-04-03 18:37:51 +0300

    Apply new notifications model changes

--- fuck_book/notifications/views.py
+++ fuck_book/notifications/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
 
 
-from notifications.models import NotificationTypes, NotificationInstance
+from notifications.models import NotificationInstance
 
 
 @login_required
@@ -11,29 +11,23 @@ def notifications(request):
     current_person = request.user.person
 
     notifications_by_type = dict()
-    for notification_type in NotificationTypes.objects.all():
-            notifications_by_type[notification_type.name]= NotificationInstance.objects.filter(person=current_person).filter(notification_type=notification_type).all()
+    for notification_type in current_person.notification_settings.keys():
+        notifications_by_type[notification_type]= NotificationInstance.objects.filter(person=current_person).filter(notification_type__name=notification_type).all()
 
     if request.method == 'POST':
         notification_key = int(request.POST.get('notification_id'))
-        notification_cls = request.POST.get('notification_class')
-        mark_notification_as_read(notification_key, notification_cls)
+        mark_notification_as_read(notification_key)
     csrf(request)
     return render(request, "notifications.html", locals())
 
 
-def mark_notification_as_read(key, cls):
+def mark_notification_as_read(key):
     ''' Takes:
     - key - an id of the notification the user wants to mark as read
     - cls - the class of this same notification
     Updates the notification's unread field from True to False so
     it won't be displayd as new any more.
     '''
-#    if cls == "PeriodicalNotification":
-#        notification = PeriodicalNotification.objects.get(id=key)
-#    elif cls == "TipNotification":
-#        notification = TipNotification.objects.get(id=key)
-#    else:
-#        notification = DifferenceNotification.objects.get(id=key)
-#    notification.unread = False
-#    notification.save(update_fields=['unread'])
+    notification = NotificationInstance.objects.get(id=key)
+    notification.is_read = True
+    notification.save(update_fields=['is_read'])
